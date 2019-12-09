93b12840e210fc33a9e140da5e38943772c8bf5b
olegrybtsov <oleg.rybtsov@gmail.com>
2019-12-09 22:50:12 +0200

    Update app.js

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,12 +1,12 @@
-var express = require('express');
+//importing node framework
+var express = require(‘express’);
+ 
 var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+//Respond with "hello world" for requests that hit our root "/"
+app.get(‘/’, function (req, res) {
+ res.send(‘hello world’);
 });
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
-
+//listen to port 3000 by default
+app.listen(process.env.PORT || 3000);
+ 
 module.exports = app;
